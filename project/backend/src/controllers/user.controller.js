be2159db91660c13d20bc83bed6dc550758b6af5
utso1707097 <utso097.csekuet@gmail.com>
2024-08-10 19:23:16 +0600

    MongoDB aggregation pipeline written

--- project/backend/src/controllers/user.controller.js
+++ project/backend/src/controllers/user.controller.js
@@ -345,6 +345,80 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+  const {username} = req.params;
+
+  if(!username?.trim()){
+    throw new ApiError(400,"username is missing");
+  }
+
+  //Find subscribers of the channel
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel", // subscribers
+        as: "subscribers"
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber", // subscribed to 
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addField: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+      }
+    }
+  ]);
+  
+  console.log(channel); // array of one object
+  if(!channel?.length){
+    throw new ApiError(404,"channel does not exists")
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+
+});
+
 export {
   registerUser,
   loginUser,
@@ -354,4 +428,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
