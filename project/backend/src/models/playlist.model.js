015749d5a8c3f8b101397a65093944ac8c474990
utso1707097 <utso097.csekuet@gmail.com>
2024-08-10 22:38:39 +0600

    Add comment like playlist tweet models

---
+++ project/backend/src/models/playlist.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+},{timestamps: true});
+
+
+export const Playlist = mongoose.model("Playlist",Playlist);
